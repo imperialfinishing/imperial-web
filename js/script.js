8102f7232f11ea6fcdd4374ce08ea3a3a405c56e
imperialfinishing <imperialfinishing01@gmail.com>
2026-07-13 08:15:56 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -170,15 +170,6 @@ document.addEventListener("keydown",(e)=>{
 
 });
 
-if (topBtn) {
-    topBtn.addEventListener("click", function () {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth"
-        });
-    });
-}
-
 // ==========================
 // STICKY NAVBAR
 // ==========================
